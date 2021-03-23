ebf32e38344e433c0e0a0da464c12d69df96c728
Abhishek kumar verma <50478316+B4xAbhishek@users.noreply.github.com>
2021-03-23 19:25:43 +0530

    tickly tots

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://admin:Kanaklata@123@cluster0.wm39w.mongodb.net/tiktok?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
