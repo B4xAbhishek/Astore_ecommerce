840cf57621db4c6d27112d3c71b81e0071f187c0
Abhishek Kumar Verma <50478316+B4xAbhishek@users.noreply.github.com>
2022-12-31 13:56:31 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://admin:Kanaklata@123@cluster0.wm39w.mongodb.net/tiktok?retryWrites=true&w=majority", {
+    const conn = await mongoose.connect("mongodb+srv://admin:@cluster0.wm39w.mongodb.net/tiktok?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
